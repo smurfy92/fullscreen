dee19c70b5aa85f35e956c549d79920c6bc6bff2
smurfy92 <tranchierjules@gmail.com>
2019-08-23 20:08:21 +0200

    clean

--- content.js
+++ content.js
@@ -1,6 +1,32 @@
-var clickedEl = null;
+// var clickedEl = null;
+
+// var myConfObj = {
+//     iframeMouseOver : false
+//   }
+
+// window.addEventListener("click", (event) => {
+//     clickedEl = event.target
+// })
+
+// window.addEventListener('blur',(event) => {
+//     clickedEl = event.target
+//     if(myConfObj.iframeMouseOver){
+//         console.log('Wow! Iframe Click!');
+//     }
+// });
+
+
+// const getIframe = (elem) => {
+//     if(!elem)
+//         return null;
+//     if (elem.nodeName === "IFRAME")
+//         return elem;
+//     return (getIframe(elem.parentNode))
+// }
+  
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // console.log("iframe?", getIframe(clickedEl));
     var iframe = document.getElementsByClassName("player-img-iframe")
     iframe[0].requestFullscreen();
 });
